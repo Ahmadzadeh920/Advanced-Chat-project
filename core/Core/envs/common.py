553f2b444be2c772c8398a98d0df369e8bee270e
Ahmadzadeh920 <f.ahmadzadeh920@gmail.com>
2024-09-08 16:08:03 +0200

    staticfiles

--- core/Core/envs/common.py
+++ core/Core/envs/common.py
@@ -158,7 +158,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"  # For Deployment
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # example path where collected files are stored
+
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'staticfiles/'),
+)
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "mediafiles"
