bf909fc4ef282a1ea4e789ff9f471dd94ed33b5a
Ahmadzadeh920 <Ahmadzadeh920@users.noreply.github.com>
2024-09-22 11:04:45 +0330

    create model for Chat group

--- core/ChatApp/models.py
+++ core/ChatApp/models.py
@@ -7,12 +7,24 @@ from accounts.models import Profile , CustomUser
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128 , unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.group_name
     
+    
     def get_absolute_api_url(self):
         return reverse("ChatApp:api-v1:group-messages-retrieve", kwargs={"pk": self.id})
+    
+    
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(ChatGroup, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('group', 'user')
     
 
 class GroupMessage(models.Model):
